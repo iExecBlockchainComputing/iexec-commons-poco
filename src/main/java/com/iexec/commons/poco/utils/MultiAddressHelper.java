a2beac55c30b327345d22defa9ed5785969e2377
Jeremy Bernard <jeremy.bernard@iex.ec>
2023-06-20 12:40:46 +0200

    Add IPFS gateways list and replace `convertToURI` with `isMultiAddress`

--- src/main/java/com/iexec/commons/poco/utils/MultiAddressHelper.java
+++ src/main/java/com/iexec/commons/poco/utils/MultiAddressHelper.java
@@ -17,24 +17,31 @@
 package com.iexec.commons.poco.utils;
 
 import io.ipfs.multiaddr.MultiAddress;
-import org.web3j.utils.Numeric;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
 
 public class MultiAddressHelper {
 
-    private static final String IPFS_GATEWAY_URI = "https://gateway.ipfs.io";
+    public static final List<String> IPFS_GATEWAYS = List.of(
+            "https://ipfs-gateway.v8-bellecour.iex.ec",
+            "https://gateway.ipfs.io",
+            "https://gateway.pinata.cloud"
+    );
 
     private MultiAddressHelper() {
         throw new UnsupportedOperationException();
     }
 
-    public static String convertToURI(String hexaString) {
-        // Currently, only the IPFS format is supported in the multiaddress field in the smart contract, so
-        // if the MultiAddress object can't be constructed, it means it is an HTTP address.
+    public static boolean isMultiAddress(String uri) {
+        if (StringUtils.isBlank(uri)) {
+            return false;
+        }
         try {
-            MultiAddress address = new MultiAddress(Numeric.hexStringToByteArray(hexaString));
-            return IPFS_GATEWAY_URI + address;
+            new MultiAddress(uri);
+            return true;
         } catch (Exception e) {
-            return BytesUtils.hexStringToAscii(hexaString);
+            return false;
         }
     }
 }
