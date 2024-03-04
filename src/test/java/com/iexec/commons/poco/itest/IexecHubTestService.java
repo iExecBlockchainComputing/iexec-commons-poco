f12543afddcf9c5ecf47d9d801c407ae9c3c8bd8
Jeremy Bernard <jeremy.bernard@iex.ec>
2024-03-04 16:36:13 +0100

    Add methods to `IexecHubTestService` and add `OrdersService` for tests

--- src/test/java/com/iexec/commons/poco/itest/IexecHubTestService.java
+++ src/test/java/com/iexec/commons/poco/itest/IexecHubTestService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 IEXEC BLOCKCHAIN TECH
+ * Copyright 2023-2024 IEXEC BLOCKCHAIN TECH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,12 +17,71 @@
 package com.iexec.commons.poco.itest;
 
 import com.iexec.commons.poco.chain.IexecHubAbstractService;
+import com.iexec.commons.poco.chain.SignerService;
 import com.iexec.commons.poco.chain.Web3jAbstractService;
+import com.iexec.commons.poco.encoding.AssetDataEncoder;
+import lombok.extern.slf4j.Slf4j;
 import org.web3j.crypto.Credentials;
+import org.web3j.utils.Numeric;
 
+import java.math.BigInteger;
+
+@Slf4j
 public class IexecHubTestService extends IexecHubAbstractService {
+    private static final BigInteger GAS_PRICE = BigInteger.valueOf(22_000_000_000L);
+    private static final BigInteger GAS_LIMIT = BigInteger.valueOf(1_000_000L);
     private static final String IEXEC_HUB_ADDRESS = "0xC129e7917b7c7DeDfAa5Fff1FB18d5D7050fE8ca";
+
+    private final SignerService signerService;
+
     public IexecHubTestService(Credentials credentials, Web3jAbstractService web3jAbstractService) {
         super(credentials, web3jAbstractService, IEXEC_HUB_ADDRESS);
+        signerService = new SignerService(
+                web3jAbstractService.getWeb3j(), web3jAbstractService.getChainId(), credentials);
+    }
+
+    public String submitCreateAppTx(BigInteger nonce, String name) throws Exception {
+        String appRegistryAddress = getHubContract().appregistry().send();
+        String appTxData = AssetDataEncoder.encodeApp(
+                signerService.getAddress(),
+                name,
+                "DOCKER",
+                "multiAddress",
+                Numeric.toHexStringNoPrefixZeroPadded(BigInteger.ZERO, 64),
+                "{}"
+        );
+        String appTxHash = signerService.signAndSendTransaction(
+                nonce, GAS_PRICE, GAS_LIMIT, appRegistryAddress, appTxData
+        );
+        log.info("app tx hash {}", appTxHash);
+        return appTxHash;
+    }
+
+    public String submitCreateDatasetTx(BigInteger nonce, String name) throws Exception {
+        String datasetRegistryAddress = getHubContract().datasetregistry().send();
+        String datasetTxData = AssetDataEncoder.encodeDataset(
+                signerService.getAddress(),
+                name,
+                "multiAddress",
+                Numeric.toHexStringNoPrefixZeroPadded(BigInteger.ZERO, 64)
+        );
+        String datasetTxHash = signerService.signAndSendTransaction(
+                nonce, GAS_PRICE, GAS_LIMIT, datasetRegistryAddress, datasetTxData
+        );
+        log.info("dataset tx hash {}", datasetTxHash);
+        return datasetTxHash;
+    }
+
+    public String submitCreateWorkerpoolTx(BigInteger nonce, String name) throws Exception {
+        String workerpoolRegistryAddress = getHubContract().workerpoolregistry().send();
+        String workerpoolTxData = AssetDataEncoder.encodeWorkerpool(
+                signerService.getAddress(),
+                name
+        );
+        String workerpoolTxHash = signerService.signAndSendTransaction(
+                nonce, GAS_PRICE, GAS_LIMIT, workerpoolRegistryAddress, workerpoolTxData
+        );
+        log.info("workerpool tx hash {}", workerpoolTxHash);
+        return workerpoolTxHash;
     }
 }
--- src/test/java/com/iexec/commons/poco/itest/MatchOrdersTests.java
+++ src/test/java/com/iexec/commons/poco/itest/MatchOrdersTests.java
@@ -17,17 +17,13 @@
 package com.iexec.commons.poco.itest;
 
 import com.iexec.commons.poco.chain.ChainDeal;
-import com.iexec.commons.poco.chain.DealParams;
 import com.iexec.commons.poco.chain.SignerService;
 import com.iexec.commons.poco.contract.generated.IexecHubContract;
-import com.iexec.commons.poco.eip712.EIP712Domain;
-import com.iexec.commons.poco.eip712.entity.EIP712AppOrder;
-import com.iexec.commons.poco.eip712.entity.EIP712DatasetOrder;
-import com.iexec.commons.poco.eip712.entity.EIP712RequestOrder;
-import com.iexec.commons.poco.eip712.entity.EIP712WorkerpoolOrder;
-import com.iexec.commons.poco.encoding.AssetDataEncoder;
 import com.iexec.commons.poco.encoding.MatchOrdersDataEncoder;
-import com.iexec.commons.poco.order.*;
+import com.iexec.commons.poco.order.AppOrder;
+import com.iexec.commons.poco.order.DatasetOrder;
+import com.iexec.commons.poco.order.RequestOrder;
+import com.iexec.commons.poco.order.WorkerpoolOrder;
 import com.iexec.commons.poco.utils.BytesUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
@@ -37,7 +33,6 @@ import org.testcontainers.containers.ComposeContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.web3j.crypto.Credentials;
-import org.web3j.crypto.Hash;
 import org.web3j.crypto.WalletUtils;
 import org.web3j.crypto.exception.CipherException;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
@@ -46,9 +41,7 @@ import org.web3j.utils.Numeric;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigInteger;
-import java.util.Map;
 import java.util.Optional;
-import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 
 import static com.iexec.commons.poco.itest.ChainTests.SERVICE_NAME;
@@ -65,11 +58,11 @@ class MatchOrdersTests {
     private static final BigInteger GAS_PRICE = BigInteger.valueOf(22_000_000_000L);
     private static final String IEXEC_HUB_ADDRESS = "0xC129e7917b7c7DeDfAa5Fff1FB18d5D7050fE8ca";
 
-    private final EIP712Domain domain = new EIP712Domain(65535L, IEXEC_HUB_ADDRESS);
     private Credentials credentials;
     private SignerService signerService;
     private IexecHubTestService iexecHubService;
     private Web3jTestService web3jService;
+    private OrdersService ordersService;
 
     @Container
     static ComposeContainer environment = new ComposeContainer(new File("docker-compose.yml"))
@@ -83,6 +76,7 @@ class MatchOrdersTests {
         web3jService = new Web3jTestService(chainNodeAddress);
         iexecHubService = new IexecHubTestService(credentials, web3jService);
         signerService = new SignerService(web3jService.getWeb3j(), web3jService.getChainId(), credentials);
+        ordersService = new OrdersService(signerService);
     }
 
     @Test
@@ -112,19 +106,20 @@ class MatchOrdersTests {
         String workerpoolAddress = iexecHubService.createWorkerpool("my-workerpool-1");
         assertThat(workerpoolAddress).isEqualTo("0x1d9340e5c33c6d289250df3b1ce31bc3ac6b16c0");
 
-        final AppOrder signedAppOrder = buildSignedAppOrder(appAddress);
+        final AppOrder signedAppOrder = ordersService.buildSignedAppOrder(appAddress);
         assertThat(signedAppOrder.getSign())
                 .isEqualTo("0xbcb64941a9e7162f6f3a2b9e636596eec1bee8d420556469e11641905b45c7785691c310e4cce295311ebf6439acf2a6a943fb8d5db87a1971f979a83e6ea34d1b");
 
-        final DatasetOrder signedDatasetOrder = buildSignedDatasetOrder(datasetAddress);
+        final DatasetOrder signedDatasetOrder = ordersService.buildSignedDatasetOrder(datasetAddress);
         assertThat(signedDatasetOrder.getSign())
                 .isEqualTo("0x014011344b5684761605fd34ecdcebd34b1efc249658a1f975d8c2a26a7adc5b68dcda7bd298cc578aea99877b5ae4e7b0f3cacd307cd4fdfb382d87a0fdc5e11c");
 
-        final WorkerpoolOrder signedWorkerpoolOrder = buildSignedWorkerpoolOrder(workerpoolAddress);
+        final WorkerpoolOrder signedWorkerpoolOrder = ordersService.buildSignedWorkerpoolOrder(workerpoolAddress);
         assertThat(signedWorkerpoolOrder.getSign())
                 .isEqualTo("0xb3b4cc51859bf106562baecf6261bfa8e168a11e06dcc82b91d0ccfbe16be14c414526c4a2e0908ef61eae6a91ec96a8d977fda4b204ac06d1b7e477abe8ea8d1b");
 
-        final RequestOrder signedRequestOrder = buildSignedRequestOrder(signedAppOrder, signedDatasetOrder, signedWorkerpoolOrder);
+        final RequestOrder signedRequestOrder = ordersService.buildSignedRequestOrder(
+                signedAppOrder, signedDatasetOrder, signedWorkerpoolOrder);
         assertThat(signedRequestOrder.getSign())
                 .isEqualTo("0xa6a6eea2c49c7df388d8a265926f19bf4cac049f73875f1dbbe813ce088a7e833a2552cb33a14927b350c858349d3bcedbe23286edf912ad585dc102a1249e751c");
 
@@ -153,43 +148,11 @@ class MatchOrdersTests {
     @Test
     void shouldMatchOrdersWithSignerService() throws Exception {
         BigInteger nonce = web3jService.getNonce(credentials.getAddress());
-        String appRegistryAddress = iexecHubService.getHubContract().appregistry().send();
-        String appTxData = AssetDataEncoder.encodeApp(
-                credentials.getAddress(),
-                "my-ap-2",
-                "DOCKER",
-                "multiAddress",
-                Numeric.toHexStringNoPrefixZeroPadded(BigInteger.ZERO, 64),
-                "{}"
-        );
-        String appTxHash = signerService.signAndSendTransaction(
-                nonce, GAS_PRICE, GAS_LIMIT, appRegistryAddress, appTxData
-        );
-        log.info("app tx hash {}", appTxHash);
-
+        final String appTxHash = iexecHubService.submitCreateAppTx(nonce, "my-ap-2");
         nonce = nonce.add(BigInteger.ONE);
-        String datasetRegistryAddress = iexecHubService.getHubContract().datasetregistry().send();
-        String datasetTxData = AssetDataEncoder.encodeDataset(
-                credentials.getAddress(),
-                "my-dataset-2",
-                "multiAddress",
-                Numeric.toHexStringNoPrefixZeroPadded(BigInteger.ZERO, 64)
-        );
-        String datasetTxHash = signerService.signAndSendTransaction(
-                nonce, GAS_PRICE, GAS_LIMIT, datasetRegistryAddress, datasetTxData
-        );
-        log.info("dataset tx hash {}", datasetTxHash);
-
+        final String datasetTxHash = iexecHubService.submitCreateDatasetTx(nonce, "my-dataset-2");
         nonce = nonce.add(BigInteger.ONE);
-        String workerpoolRegistryAddress = iexecHubService.getHubContract().workerpoolregistry().send();
-        String workerpoolTxData = AssetDataEncoder.encodeWorkerpool(
-                credentials.getAddress(),
-                "my-workerpool-2"
-        );
-        String workerpoolTxHash = signerService.signAndSendTransaction(
-                nonce, GAS_PRICE, GAS_LIMIT, workerpoolRegistryAddress, workerpoolTxData
-        );
-        log.info("workerpool tx hash {}", workerpoolTxHash);
+        final String workerpoolTxHash = iexecHubService.submitCreateWorkerpoolTx(nonce, "my-workerpool-2");
 
         TimeUnit.SECONDS.sleep(5L);
         String appAddress = decodeReceipt(web3jService.getTransactionReceipt(appTxHash));
@@ -200,19 +163,20 @@ class MatchOrdersTests {
         assertThat(datasetAddress).isEqualTo("0xb076cc627fb78ab33bb328f43e316547f6c26edc");
         assertThat(workerpoolAddress).isEqualTo("0x0bec41f6e1424340f252b3687a3686a86134ebe3");
 
-        final AppOrder signedAppOrder = buildSignedAppOrder(appAddress);
+        final AppOrder signedAppOrder = ordersService.buildSignedAppOrder(appAddress);
         assertThat(signedAppOrder.getSign())
                 .isEqualTo("0xa86bb2ea025db10e3ba70b8defcd3009a4afee6b39b95a357a62a7cf07b757db76fb0f6dbf4e409262e7a1a8f1695ecdcaca0b465cebd1c77d4b97a6f102a2201b");
 
-        final DatasetOrder signedDatasetOrder = buildSignedDatasetOrder(datasetAddress);
+        final DatasetOrder signedDatasetOrder = ordersService.buildSignedDatasetOrder(datasetAddress);
         assertThat(signedDatasetOrder.getSign())
                 .isEqualTo("0xfa2bf8bbfde7db118970aae8781dc78210bd9433111f1332f549aa003f7f28cc6c68c038aa35b5ddfc98242a8ede07ea8fa073f2b0e7f1247b8b97fadc4837c91c");
 
-        final WorkerpoolOrder signedWorkerpoolOrder = buildSignedWorkerpoolOrder(workerpoolAddress);
+        final WorkerpoolOrder signedWorkerpoolOrder = ordersService.buildSignedWorkerpoolOrder(workerpoolAddress);
         assertThat(signedWorkerpoolOrder.getSign())
                 .isEqualTo("0xf6474336a10e3ec12ff65e721fee6ba53763f937a1b54659332f61cf1770b4b26947df22ad89e8623aa985763e917e9f66670e51befc7195b16b0e8db365c13c1c");
 
-        final RequestOrder signedRequestOrder = buildSignedRequestOrder(signedAppOrder, signedDatasetOrder, signedWorkerpoolOrder);
+        final RequestOrder signedRequestOrder = ordersService.buildSignedRequestOrder(
+                signedAppOrder, signedDatasetOrder, signedWorkerpoolOrder);
         assertThat(signedRequestOrder.getSign())
                 .isEqualTo("0x6b38847f28bd7249823a7b99538ff6cca4a2cd952d5860f062be8142b3defcd13fad163a10e6de9e219981a333d3fbd554532be07263b7d77cbd48b2803413051c");
 
@@ -236,85 +200,6 @@ class MatchOrdersTests {
         String chainDealId = BytesUtils.bytesToString(dealid);
         Optional<ChainDeal> oChainDeal = iexecHubService.getChainDeal(chainDealId);
         assertThat(oChainDeal).isPresent();
-    }
-
-    private AppOrder buildSignedAppOrder(String appAddress) {
-        final AppOrder appOrder = AppOrder.builder()
-                .app(appAddress)
-                .appprice(BigInteger.ZERO)
-                .volume(BigInteger.ONE)
-                .tag(OrderTag.TEE_SCONE.getValue())
-                .datasetrestrict(BytesUtils.EMPTY_ADDRESS)
-                .workerpoolrestrict(BytesUtils.EMPTY_ADDRESS)
-                .requesterrestrict(BytesUtils.EMPTY_ADDRESS)
-                .salt(Hash.sha3String("abcd"))
-                .build();
-        final String sig = signerService.signEIP712Entity(new EIP712AppOrder(domain, appOrder));
-        return appOrder.withSignature(sig);
-    }
-
-    private DatasetOrder buildSignedDatasetOrder(String datasetAddress) {
-        final DatasetOrder datasetOrder = DatasetOrder.builder()
-                .dataset(datasetAddress)
-                .datasetprice(BigInteger.ZERO)
-                .volume(BigInteger.ONE)
-                .tag(OrderTag.TEE_SCONE.getValue())
-                .apprestrict(BytesUtils.EMPTY_ADDRESS)
-                .workerpoolrestrict(BytesUtils.EMPTY_ADDRESS)
-                .requesterrestrict(BytesUtils.EMPTY_ADDRESS)
-                .salt(Hash.sha3String("abcd"))
-                .build();
-        final String sig = signerService.signEIP712Entity(new EIP712DatasetOrder(domain, datasetOrder));
-        return datasetOrder.withSignature(sig);
-    }
-
-    private WorkerpoolOrder buildSignedWorkerpoolOrder(String workerpoolAddress) {
-        final WorkerpoolOrder workerpoolOrder = WorkerpoolOrder.builder()
-                .workerpool(workerpoolAddress)
-                .workerpoolprice(BigInteger.TEN)
-                .volume(BigInteger.ONE)
-                .tag(OrderTag.TEE_SCONE.getValue())
-                .category(BigInteger.ZERO)
-                .trust(BigInteger.ONE)
-                .apprestrict(BytesUtils.EMPTY_ADDRESS)
-                .datasetrestrict(BytesUtils.EMPTY_ADDRESS)
-                .requesterrestrict(BytesUtils.EMPTY_ADDRESS)
-                .salt(Hash.sha3String("abcd"))
-                .build();
-        final String sig = signerService.signEIP712Entity(new EIP712WorkerpoolOrder(domain, workerpoolOrder));
-        return workerpoolOrder.withSignature(sig);
-    }
-
-    private RequestOrder buildSignedRequestOrder(AppOrder appOrder, DatasetOrder datasetOrder, WorkerpoolOrder workerpoolOrder) {
-        final TreeMap<String, String> iexecSecrets = new TreeMap<>(Map.of(
-                "1", "first-secret",
-                "2", "second-secret",
-                "3", "third-secret"));
-        final DealParams dealParams = DealParams.builder()
-                .iexecResultEncryption(true)
-                .iexecResultStorageProvider("ipfs")
-                .iexecResultStorageProxy("http://result-proxy:13200")
-                .iexecSecrets(iexecSecrets)
-                .build();
-        final RequestOrder requestOrder = RequestOrder.builder()
-                .app(appOrder.getApp())
-                .appmaxprice(appOrder.getAppprice())
-                .dataset(datasetOrder.getDataset())
-                .datasetmaxprice(datasetOrder.getDatasetprice())
-                .workerpool(workerpoolOrder.getWorkerpool())
-                .workerpoolmaxprice(workerpoolOrder.getWorkerpoolprice())
-                .requester(credentials.getAddress())
-                .volume(BigInteger.ONE)
-                .tag(OrderTag.TEE_SCONE.getValue())
-                .category(BigInteger.ZERO)
-                .trust(BigInteger.ONE)
-                .beneficiary(credentials.getAddress())
-                .callback(BytesUtils.EMPTY_ADDRESS)
-                .params(dealParams.toJsonString())
-                .salt(Hash.sha3String("abcd"))
-                .build();
-        final String sig = signerService.signEIP712Entity(new EIP712RequestOrder(domain, requestOrder));
-        return requestOrder.withSignature(sig);
     }
 
     private String decodeReceipt(TransactionReceipt receipt) {
